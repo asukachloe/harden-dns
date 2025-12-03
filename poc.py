ae7a45e3fc13beea33d61608387731a9a4eaf37e
Asuka Chloe ⚡ <31380858+asukachloe@users.noreply.github.com>
2025-12-03 22:35:15 +0530

    Create poc.py

---
+++ poc.py
@@ -0,0 +1,26 @@
+import socket
+import struct
+import sys
+
+def encode_domain(domain):
+    parts = domain.split('.')
+    encoded = b''.join(struct.pack('B', len(part)) + part.encode() for part in parts) + b'\x00'
+    return encoded
+
+def build_query(domain):
+    id_val = 0x1234
+    header = struct.pack('>HHH', id_val, 0x0100, 1) + struct.pack('>HHH', 0, 0, 0)
+    qname = encode_domain(domain)
+    question = qname + struct.pack('>HH', 1, 1)
+    message = header + question
+    return message
+
+domains = sys.argv[1:]
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('8.8.8.8', 53))
+for domain in domains:
+    msg = build_query(domain)
+    length = len(msg)
+    prefix = struct.pack('>H', length)
+    sock.sendall(prefix + msg)
+sock.close()
